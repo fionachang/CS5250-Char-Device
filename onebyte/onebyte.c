32836c17b88948522fefd20bdc45ae3bced599b5
fionachang <fiona.chang.w@gmail.com>
2017-03-16 16:09:23 +0800

    Update length

--- onebyte/onebyte.c
+++ onebyte/onebyte.c
@@ -9,7 +9,7 @@
 #include <asm/uaccess.h>
 
 #define MAJOR_NUMBER 61
-#define SIZE 1
+#define LEN 2  // length of onebyte_data and null terminator
 
 /* forward declaration */
 int onebyte_open(struct inode *inode, struct file *filep);
@@ -41,26 +41,26 @@ int onebyte_release(struct inode *inode, struct file *filep)
 
 ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
 {
-    if (copy_to_user(buf, onebyte_data, SIZE)) {
+    if (copy_to_user(buf, onebyte_data, LEN)) {
         return -EFAULT;
     }
     
-    *f_pos += SIZE;
-    count -= SIZE;
+    *f_pos += LEN;
+    count -= LEN;
     
     printk(KERN_ALERT "%s", onebyte_data);
     
-    return SIZE;
+    return LEN;
 }
 
 ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
 {
-    if (copy_from_user(onebyte_data, buf, SIZE)) {
+    if (copy_from_user(onebyte_data, buf, LEN)) {
         return -EFAULT;
     }
     
-    *f_pos += SIZE;
-    count -= SIZE;
+    *f_pos += LEN;
+    count -= LEN;
     
     printk(KERN_ALERT "%lu", count);
     
@@ -68,7 +68,7 @@ ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t 
         return -ENOSPC;
     }
     
-    return SIZE;
+    return LEN;
 }
 
 static int onebyte_init(void)
